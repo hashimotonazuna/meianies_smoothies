1b32bb643e526b870673f250ae8e03d38a5df831
hashimotonazuna <hashimoto.nazuna.4880@members.co.jp>
2024-11-01 13:22:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,10 @@ if ingredients_list:
                 fv_df = st.dataframe (data=fruitvice_response/json(), use_container_width=True)
 
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
     
-time_to_insert = st.button ('Submit Order')
+        time_to_insert = st.button ('Submit Order')
 
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
