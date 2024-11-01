3626d708c8e8de3ae844619a0058e9127ba6cf5c
hashimotonazuna <hashimoto.nazuna.4880@members.co.jp>
2024-11-01 11:24:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -22,10 +23,12 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 if ingredients_list:
-    ingredients_string = ''
+        ingredients_string = ''
     
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+        for fruit_chosen in ingredients_list:
+                ingredients_string += fruit_chosen + ' '
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+                fv_df = st.dataframe (data=fruitvice_response/json(), use_container_width=True)
 
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -38,7 +41,5 @@ if time_to_insert:
     
     st.success('Your Smoothie is ordered!', icon="✅")
 
-#New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
+
